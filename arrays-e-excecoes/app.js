ef89374ea7aa9f6771de8aa168a5b01ba3d61c97
Sergio Pereira Sena <senanetworker@gmail.com>
2023-03-14 17:15:33 -0300

    Update estudos JScript

--- arrays-e-excecoes/app.js
+++ arrays-e-excecoes/app.js
@@ -1,17 +1,32 @@
-const listaDeProdutos = [
-    "Pão",
-    "leite",
-    "cafe",
-    "laranja",
-    "Macarrão",
-    "Sabonete",
-    "Deternte",
-];
-
-const listaDeArgumentos = process.argv.slice(2);
-
-const listaDeProdutosSolicitadosDisponiveis = listaDeProdutos.filter(produto => {
-    return listaDeArgumentos.find(argumento => argumento === produto);
-});
-
-listaDeProdutosSolicitadosDisponiveis.forEach(produto => console.log(`Estes produtos nos temos: ${produto}`))
+try {
+    const listaDeProdutosDisponiveis = [
+        "Pão",
+        "leite",
+        "Café",
+        "Laranja",
+        "Macarrão",
+        "Sabonete",
+        "Detergente"
+    ];
+    
+    const listaDeArgumentos = process.argv.slice(2);
+    
+    const listaDeProdutosSolicitadosDisponiveis = listaDeProdutosDisponiveis.filter(produto => {
+        return listaDeArgumentos.find(argumento => argumento === produto);
+    })
+    
+    listaDeProdutosSolicitadosDisponiveis.forEach(produto => console.log(`Esses produtos nos temos: ${produto}`));
+    
+    const listaDeProdutosSolicitadosNaoDisponiveis = listaDeArgumentos.filter(argumento => {
+        return !listaDeProdutosDisponiveis.find(produto  => produto === argumento);
+    })
+    
+    listaDeProdutosSolicitadosNaoDisponiveis.forEach(argumento => console.log(`Esse produtos nós não temos: ${argumento}`));
+    
+    listaDeProdutosDisponiveis.sort();
+    listaDeProdutosDisponiveis.forEach(produto => console.log(`Esse prosuto esta disponovel: ${produto}`));
+    
+} catch (e) {
+    console.log(`Nao foi possivel completar a sua lista. Contate o ADB`);
+    
+}
